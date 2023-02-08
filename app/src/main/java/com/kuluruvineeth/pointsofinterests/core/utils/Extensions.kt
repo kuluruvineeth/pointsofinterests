4241ee0cdbc2d7fcd2ad1e9771fdcdb32821fc47
Kuluruvineeth <kuluruvineeth8623@gmail.com>
2023-02-08 08:33:16 +0530

    - Completed about app screen

--- app/src/main/java/com/kuluruvineeth/pointsofinterests/core/utils/Extensions.kt
+++ app/src/main/java/com/kuluruvineeth/pointsofinterests/core/utils/Extensions.kt
@@ -1,8 +1,14 @@
 package com.kuluruvineeth.pointsofinterests.core.utils
 
+import android.content.Context
+import android.net.Uri
 import androidx.annotation.StringRes
+import androidx.browser.customtabs.CustomTabColorSchemeParams
+import androidx.browser.customtabs.CustomTabsIntent
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.ui.graphics.toArgb
 import com.kuluruvineeth.pointsofinterests.features.categories.ui.models.CategoryUiModel
 
 /*
@@ -15,3 +21,27 @@ fun stringFromResource(
  */
 
 fun List<CategoryUiModel>.containsId(id: String) = this.any { it.id == id }
+
+@Composable
+fun chromeTabsIntent(): CustomTabsIntent {
+    val builder = CustomTabsIntent.Builder()
+    val colorSchemaParams = CustomTabColorSchemeParams.Builder()
+        .setToolbarColor(MaterialTheme.colorScheme.background.toArgb())
+        .setSecondaryToolbarColor(MaterialTheme.colorScheme.secondary.toArgb())
+        .setNavigationBarColor(MaterialTheme.colorScheme.background.toArgb())
+        .setNavigationBarDividerColor(MaterialTheme.colorScheme.onBackground.copy(
+            alpha = 0.2f
+        ).toArgb())
+        .build()
+    builder.setDefaultColorSchemeParams(colorSchemaParams)
+    return builder.build()
+}
+
+
+/**
+ * Shorter version of [CustomTabsIntent.launchUrl],
+ * created to launch all any intent in the same code style.
+ */
+fun CustomTabsIntent.launch(context: Context, url: String){
+    launchUrl(context, Uri.parse(url))
+}
--- app/src/main/java/com/kuluruvineeth/pointsofinterests/features/about/AboutScreen.kt
+++ app/src/main/java/com/kuluruvineeth/pointsofinterests/features/about/AboutScreen.kt
@@ -1,8 +1,11 @@
 package com.kuluruvineeth.pointsofinterests.features.about
 
 import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Icon
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
@@ -11,15 +14,24 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.vector.ImageVector
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.kuluruvineeth.pointsofinterests.BuildConfig
 import com.kuluruvineeth.pointsofinterests.R
+import com.kuluruvineeth.pointsofinterests.core.utils.chromeTabsIntent
+import com.kuluruvineeth.pointsofinterests.core.utils.launch
 
 
 @Composable
 fun AboutScreen() {
+
+    val context = LocalContext.current
     Column(
         modifier = Modifier
             .padding(48.dp)
@@ -44,7 +56,8 @@ fun AboutScreen() {
             Spacer(modifier = Modifier.size(32.dp))
             Text(
                 text = stringResource(id = R.string.app_name).uppercase(),
-                style = MaterialTheme.typography.titleLarge
+                style = MaterialTheme.typography.titleLarge,
+                color = MaterialTheme.colorScheme.onBackground
             )
             Spacer(modifier = Modifier.size(8.dp))
             Text(
@@ -55,8 +68,73 @@ fun AboutScreen() {
             )
         }
 
-        Column(modifier = Modifier.height(128.dp)) {
-            //TODO add info
+        Column(
+            modifier = Modifier
+                .height(128.dp)
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            val chromeTabsIntent = chromeTabsIntent()
+            val profileUrl = stringResource(id = R.string.url_github_profile)
+
+            Row(
+                modifier = Modifier.wrapContentSize(),
+                horizontalArrangement = Arrangement.Center,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Text(
+                    text = stringResource(id = R.string.title_developed_by),
+                    style = MaterialTheme.typography.titleSmall,
+                    fontSize = 16.sp,
+                    color = MaterialTheme.colorScheme.onBackground
+                )
+                Spacer(modifier = Modifier.size(8.dp))
+                Row(
+                    modifier = Modifier
+                        .background(MaterialTheme.colorScheme.background, RoundedCornerShape(4.dp))
+                        .border(
+                            width = 2.dp,
+                            color = MaterialTheme.colorScheme.onBackground,
+                            shape = RoundedCornerShape(4.dp)
+                        )
+                        .clip(RoundedCornerShape(4.dp))
+                        .clickable {
+                            chromeTabsIntent.launch(
+                                context, profileUrl
+                            )
+                        },
+                    horizontalArrangement = Arrangement.Center,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Icon(
+                        modifier = Modifier
+                            .size(28.dp)
+                            .padding(6.dp),
+                        imageVector = ImageVector.vectorResource(id = R.drawable.ic_github_logo),
+                        contentDescription = "Github logo",
+                        tint = MaterialTheme.colorScheme.onBackground
+                    )
+                    Text(
+                        modifier = Modifier.padding(end = 6.dp),
+                        text = stringResource(id = R.string.author_name),
+                        style = MaterialTheme.typography.titleSmall,
+                        fontSize = 12.sp,
+                        color = MaterialTheme.colorScheme.onBackground
+                    )
+                }
+            }
+            Spacer(modifier = Modifier.size(16.dp))
+            Text(
+                textAlign = TextAlign.Center,
+                text = stringResource(
+                    id = R.string.title_app_version,
+                    BuildConfig.VERSION_NAME,
+                    BuildConfig.VERSION_CODE.toString()
+                ),
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.onBackground.copy(alpha = 0.5f)
+            )
         }
     }
 }
